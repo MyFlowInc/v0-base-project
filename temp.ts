da51bdface257ef5df48cb01948c593cf3f48bf5
rouroumaki <2557654557@qq.com>
2026-03-24 16:38:23 +0800

    feat: initialize project structure with essential files and configurations

---
+++ temp.ts
@@ -0,0 +1,4 @@
+if (process.env.NODE_ENV === "development") {
+  process.env.FLOW_APP_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYXBwIiwiYmFzZUlkIjoiYnNlVUZ5R2FzVTdDNUVwOExlSiIsImlhdCI6MTc3NDMzOTA0MSwiZXhwIjoyMDg5OTE1MDQxfQ.KQJnrfqQIZ2NPHn6UiIqc4jR47g2LBl8Zx7SraDgio4";
+  process.env.FLOW_API_URL = "https://app.flow.ai";
+}
